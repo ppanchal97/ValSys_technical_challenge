b206b9aec233791d710786362dcfc85bed85646a
ppanchal97 <parik@Pariks-MacBook-Pro.local>
2020-01-13 11:34:15 +0000

    Refactored Code

--- src/lib.py
+++ src/lib.py
@@ -1,83 +1,69 @@
+# Define Imports
 from collections import Counter
 import csv
 import re
 
 
-# accepts key-value pairs and checks if it is a balance sheet or not
+# Checks if a given file is a balance sheet by computing if assets = (liabilities + equity)
 def is_balance_sheet(data):
-    # find most frequently occuring value
     x = value, count = Counter(data.values()).most_common(1)[0]
-    # print(x)
-    # look at the key of that value to check if they contain assets or liabilities
     for key, value in data.items():
         if value == x[0]:
             if "asset" in key.lower() or "liabilit" in key.lower():
-                # print(key, value)
                 return True
     return False
 
 
+# Check if a given file is an income statement by computing the net income and adjusting for deductibles
 def is_income_statement(data):
-    # initialize constants
     tax_figures = []
     nums = []
-    # find the word 'tax'
     for key, value in data.items():
         if "tax" in key.lower():
             if "income" in key.lower() or "provision" in key.lower() or "loss" in key.lower():
-                # print(list(data).index(key))
                 nums.append(key)
                 tax_figures.append(value)
-    # check if values are immediately next to each other according to GAAP
+    # check if values are immediately next to each other in accordance with GAAP principles
     if len(nums) == 2 and nums[1] == nums[0 + 1]:
-        # print('adjacent')
         # calculate the total PNL
         net_income = tax_figures[0] - abs(tax_figures[1])
-        # print(net_income)
-        # # check if the value exists in the data now
         for key, value in data.items():
             if value == net_income:
                 return True
     return False
 
 
+# Parse a CSV file into a dictionary containing key-value pairs. Hashmap values are standardized to type float
 def parse_csv(file_path):
-    # Open and parse CSV
     with open(f'{file_path}') as csv_file:
-        # define constants
         values = {}
         csv_reader = csv.reader(csv_file, delimiter=",")
-        # insert key-value pairs into hashmap
         for date, key, value in csv_reader:
             # sanitize data
-            # if fields are not empty or contain unwanted characters
             if value != "" and value != "Value" and value != "0":
-                # specify the characters to remove
                 pattern = '[$, £, \s, (, )]'
                 val = re.match(pattern, value)
-                # if you find a field that contains the specified character/s
                 if val:
-                    # replace the specified characters with empty space
+                    # remove unwanted characters
                     x = re.sub('[$, £, \s, (, )]+', '', value).strip()
-                    # insert it into the list
                     if key not in values.keys():
-                        # convert to float to make easier to deal with
                         values[key] = float(x)
                 else:
-                        # convert to float and push
                     values[key] = float(value)
     return values
 
 
+# Return the type of financial document
 def check_file(data):
     if is_balance_sheet(data):
-        print('is balance sheet')
+        print('Balance Sheet')
     elif is_income_statement(data):
-        print('is income statement')
+        print('Income Statement')
     else:
-        print('is cash-flow statement')
+        print('Cash-Flow Statement')
 
 
+# Validate terminal user input
 def check_args(args):
     if len(args) != 2:
         print("Incorrect number of arguments, Please check correct format")
